e9347d724c36584e341a90c703539f9a521060ab
victoria4055 <victoria.villasa@ufl.edu>
2024-04-10 10:32:16 -0400

    I wrote encode!

---
+++ main.py
@@ -0,0 +1,42 @@
+# Victoria Villasana 86143370
+
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)  # Shifting up by 3 numbers
+        encoded_password += encoded_digit
+    return encoded_password
+
+
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. Encode Password")
+        print("2. Decode Password")
+        print("3. Exit")
+
+        choice = input("Enter menu choice: ")
+
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            if len(password) != 8 or not password.isdigit():
+                print("Error: Password must be an 8-digit integer.")
+            else:
+                encoded_password = encode(password)
+                print("Your password has been encoded and stored")
+        elif choice == "2":
+            encoded_password = input("Enter the encoded password to decode: ")
+            if len(encoded_password) != 8 or not encoded_password.isdigit():
+                print("Error: Encoded password must be an 8-digit integer.")
+            else:
+                decoded_password = decode(encoded_password)
+                print(f"Your encoded password is {encoded_password}, and the original password is {decoded_password}))
+        elif choice == "3":
+            print()
+            break
+        else:
+            print("Invalid choice.")
+
+
+if __name__ == "__main__":
+    main()
